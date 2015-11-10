d7996093465a679e3fb56b3dfb0490f4ff727343
Anatolii Yakushko <ayakushko@intropro.com>
2015-11-10 18:12:59 +0200

    [+] added uploading to maven repo

--- src/test/java/ua/org/shaddy/anion/streamtools/bitoutputstream/BitStreamEncoderTest.java
+++ src/test/java/ua/org/shaddy/anion/streamtools/bitoutputstream/BitStreamEncoderTest.java
@@ -2,6 +2,7 @@ package ua.org.shaddy.anion.streamtools.bitoutputstream;
 
 import junit.framework.TestCase;
 import ua.org.shaddy.anion.streamtools.ByteOrder;
+import ua.org.shaddy.anion.streamtools.bitinputstream.ByteBitInputStream;
 
 public class BitStreamEncoderTest extends TestCase {
 
@@ -19,7 +20,6 @@ public class BitStreamEncoderTest extends TestCase {
 		bse.pad();
 		assertEquals((byte) 255, bos.getData()[0]);
 		assertEquals((byte) 15, bos.getData()[1]);
-		
 	}
 	
 	public void testWriteInt32(){
@@ -41,23 +41,12 @@ public class BitStreamEncoderTest extends TestCase {
 		assertEquals((byte) 0x03, bos.getData()[2]);
 		assertEquals((byte) 0x04, bos.getData()[3]);
 	}
-	
-	/*public void testWriteBigEndian(){
-		ByteBitOutputStream bos = new ByteBitOutputStream(6);
-		BitStreamEncoder bse = new BitStreamEncoder(bos, ByteOrder.BIG_ENDIAN);
-		bse.writeInt(0x01 << 24 | 0x02 << 16 | 0x03 << 8 | 0x04, 12);
-		assertEquals((byte) 0x01, bos.getData()[0]);
-		assertEquals((byte) 0x02, bos.getData()[1]);
-		assertEquals((byte) 0x03, bos.getData()[2]);
-		assertEquals((byte) 0x04, bos.getData()[3]);
-	}*/
-	
+		
 	public void testWriteLittleEndian(){
 		ByteBitOutputStream bos = new ByteBitOutputStream(6);
 		BitStreamEncoder bse = new BitStreamEncoder(bos, ByteOrder.BIG_ENDIAN);
 		bse.writeInt(0x01 << 8 | 0x02, 12);
 		assertEquals((byte) 33, bos.getData()[0]);
 		assertEquals((byte) 0, bos.getData()[1]);
-		
 	}
 }
